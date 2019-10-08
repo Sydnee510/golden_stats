211875a4397ae81de300308f7cd2fa546efd5a08
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-08 13:21:21 +0000

    initial commit

---
+++ lib/golden_stats.rb
@@ -0,0 +1,6 @@
+require "golden_stats/version"
+
+module GoldenStats
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/golden_stats_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GoldenStats do
+  it "has a version number" do
+    expect(GoldenStats::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
