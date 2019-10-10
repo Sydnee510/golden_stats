352ad03e9f414de983ce439262875de9f648613c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-10 18:12:17 +0000

    added the attributes

--- lib/golden_stats/player.rb
+++ lib/golden_stats/player.rb
@@ -1,3 +1,14 @@
 class Player 
-  attr_accessor :name, :number, :position
+  attr_accessor :name, :jersey_number, :position, :stats
+  @@all = []
+  def initialize(name, jersey_number, position, stats) 
+    @name = name 
+    @jersey_number = jersey_number
+    @position = position
+    @@all << self
+  end 
+  #just trying this out
+  #does adding these lines work 
+  #need to save my work frequent
+  #new code 
 end 
