5db3d49525401fa9a21ad2940005c680dd9d7375
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-11 08:20:07 +0000

    edited my messages in my cli class

--- lib/golden_stats/cli.rb
+++ lib/golden_stats/cli.rb
@@ -5,11 +5,11 @@ class GoldenStats::CLI
   #   input = ""
   #   while input != "exit"
      puts "Welcome to the Warriors Stats portal!"
-     puts "Please enter your faviorite player's name:"
+     puts "Please enter your faviorite player's name in this format 'first_last':"
      name = gets.chomp
      puts "You entered player \"#{name}\""
      scraper = GoldenStats::Scraper.new(name)
-     puts "here is the player"
+     puts "here is the player's stats"
      puts scraper.players
    
   #   puts "To quit, type 'exit'."
