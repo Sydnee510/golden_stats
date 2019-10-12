281b790a2e8c84e4f3c2d4fb89824cedaaffa551
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-12 22:04:47 +0000

    adding new css selectors

--- lib/golden_stats/scraper.rb
+++ lib/golden_stats/scraper.rb
@@ -1,13 +1,16 @@
-require 'pry'
-require 'player'
+#require 'player'
+
 class GoldenStats::Scraper 
   attr_reader :url
  
    def self.scrape
     doc =  Nokogiri::HTML(open("https://www.nba.com/warriors/stats/points"))
-    player = doc.css("span.playerName a").map do |anchor|
+    player = doc.css("div.player-name__inner-wrapper").each do |anchor|
       binding.pry
-      hash = { }
+       hash = { 
+        url: anchor.css("a").attr("href").value  
+        name: anchor.css("a").text
+         }
     # GoldenStats::Player.new(hash)
 
  
