f242053cfe0a8410cf8c47a153f86c505658149b
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-12 23:35:44 +0000

    adding more css selectors to pull out the player info

--- lib/golden_stats/scraper.rb
+++ lib/golden_stats/scraper.rb
@@ -6,14 +6,32 @@ class GoldenStats::Scraper
    def self.scrape
     doc =  Nokogiri::HTML(open("https://www.nba.com/warriors/stats/points"))
     player = doc.css("div.player-name__inner-wrapper").each do |anchor|
+      binding.pry
        hash = { 
         url: anchor.css("a").attr("href").value,  
-        name: anchor.css("a").text
+        name: anchor.css("a").text,
+        pl_num:
+        pl_pos:
+        games_played:
+        points:
+        fouls:
+        rebounds:
+        field_goal_per:
+        three_point_per:
+        free_throw_per:
+        assists:
+        steals:
+        turn_overs:
+        fouls:
+        
          }
      GoldenStats::Player.new(hash)
      
 
 end 
 end
+  def self.scrape_stats(info)
+    
+  end
 #   def 
 end
