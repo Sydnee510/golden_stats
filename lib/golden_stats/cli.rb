bfb1b1f6208b53799dfe82382f31345cb1743e81
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-13 02:27:44 +0000

    lining up all my methods

--- lib/golden_stats/cli.rb
+++ lib/golden_stats/cli.rb
@@ -10,37 +10,37 @@ class GoldenStats::CLI
      puts
      players
      command
-    end
+  end
     
-    def players
-      GoldenStats::Player.all.each.with_index(1) do |object, index|
-       puts "#{index}. #{object.name}".colorize(:blue)
+  def players
+    GoldenStats::Player.all.each.with_index(1) do |object, index|
+    puts "#{index}. #{object.name}".colorize(:blue)
     end
   end
   
-    def command
-      puts
-      puts "Type the number of your favorite player here!:".colorize(:magenta)
-      puts 
-      puts "You can type exit at anytime to leave the program".colorize(:red)
-      input = ""
+  def command
+    puts
+    puts "Type the number of your favorite player here!:".colorize(:magenta)
+    puts 
+    puts "You can type exit at anytime to leave the program".colorize(:red)
+    input = ""
       
-      while input != "exit, players"
-        input = gets.chomp
+    while input != "exit, players"
+      input = gets.chomp
         
-        if (1..players.size).include?(input.to_i)
-          @chosen_player = GoldenStats::Player.all[input.to_i - 1]
-          puts
-          puts "You entered player #{@chosen_player.name}".colorize(:yellow)
-          puts "His jersey number is #{@chosen_player.pl_num}".colorize(:yellow)
-          puts "His position is a #{@chosen_player.pl_pos}".colorize(:yellow)
-          puts "For #{@chosen_player.name}'s game statistics go to:".colorize(:yellow)
-          puts "#{@chosen_player.url}".colorize(:cyan)
-          puts
-          puts "Type 'players' to view the roster again:".colorize(:magenta)
-          puts "Or type the number of another player:".colorize(:magenta)
-          puts
-          puts "You can type 'exit' at any time to leave the program".colorize(:red)
+      if (1..players.size).include?(input.to_i)
+        @chosen_player = GoldenStats::Player.all[input.to_i - 1]
+        puts
+        puts "You entered player #{@chosen_player.name}".colorize(:yellow)
+        puts "His jersey number is #{@chosen_player.pl_num}".colorize(:yellow)
+        puts "His position is a #{@chosen_player.pl_pos}".colorize(:yellow)
+        puts "For #{@chosen_player.name}'s game statistics go to:".colorize(:yellow)
+        puts "#{@chosen_player.url}".colorize(:cyan)
+        puts
+        puts "Type 'players' to view the roster again:".colorize(:magenta)
+        puts "Or type the number of another player:".colorize(:magenta)
+        puts
+        puts "You can type 'exit' at any time to leave the program".colorize(:red)
           
         elsif input.downcase == "players"
           command
@@ -52,9 +52,10 @@ class GoldenStats::CLI
           puts
           puts "Sorry! Please enter a specific number from the roster!".colorize(:red)
           command
-         end
-       end
+        end
+      end
        puts "See you later!".colorize(:green)
        exit
     end 
+  
 end
