cd930f424026c271cd7cddd884b2997cb3587c34
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-16 15:35:38 +0000

    cleaned up my scraper class

--- lib/golden_stats/scraper.rb
+++ lib/golden_stats/scraper.rb
@@ -7,18 +7,12 @@ class GoldenStats::Scraper
    def self.scrape
     doc =  Nokogiri::HTML(open("https://www.nba.com/warriors/stats/points"))
    player = doc.css("div.player-name__inner-wrapper").each do |anchor|
-     #binding.pry
-    #  player = doc.css("tr").each do |anchor|
-     new_player = GoldenStats::Player.new
-        
-        new_player.url = anchor.css("a").attr("href").value,  
-        new_player.name = anchor.css("a").text,
-         new_player.pl_num =  anchor.css("span.playerNumber").text,
-         new_player.pl_pos = anchor.css("span.playerPosition").text
+        url = anchor.css("a").attr("href").value,  
+        name = anchor.css("a").text,
+         pl_num =  anchor.css("span.playerNumber").text,
+         pl_pos = anchor.css("span.playerPosition").text
 
-     #GoldenStats::Player.new(url, name, pl_num, pl_pos)
+     GoldenStats::Player.new(url, name, pl_num, pl_pos)
    end 
 end
-  
-#   def 
 end
